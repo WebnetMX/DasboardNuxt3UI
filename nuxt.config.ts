6ad863b6e0e21622a864d776fabbadaff7ce3bcc
Yusniel Brocard <brocard@gmail.com>
2023-11-23 10:33:52 -0600

    add meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,11 @@ export default defineNuxtConfig({
       },
       titleTemplate: "%s - Admin UI",
       title: "Admin UI - Webnet",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width,initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
+      ],
     },
   }
 })
