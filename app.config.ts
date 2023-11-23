0db0349a76fa7d6d9770b6d78e79f0b2ab28b788
Yusniel Brocard <brocard@gmail.com>
2023-11-23 11:42:51 -0600

    wip

--- app.config.ts
+++ app.config.ts
@@ -6,6 +6,12 @@ export default defineAppConfig({
       center: true,
       constrained: "max-w-7xl lg:max-w-7xl xl:max-w-full"
     },
+    accordion: {
+      transition: {
+        enterActiveClass: "overflow-hidden transition-[height] duration-200 ease-out",
+        leaveActiveClass: "overflow-hidden transition-[height] duration-200 ease-out"
+      },
+    },
     navigation: {
       links: {
         base: "hover:text-primary dark:hover:bg-gray-800/50 rounded-r-md",
